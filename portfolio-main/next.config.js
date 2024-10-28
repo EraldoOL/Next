b52f60cc7344554004e278f7909cef2f125afe1a
EraldoOL <eo80797@gmail.com>
2024-10-28 17:00:18 -0300

    update next.config.js

--- portfolio-main/next.config.js
+++ portfolio-main/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
+        hostname: 'https://avatars.githubusercontent.com/u/139938613?s=400&u=3307ad614c532f668543ad211dd4f7c78678da77&v=4',
         port: '',
         pathname: '/u/**',
       },
